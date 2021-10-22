16450f50e5ade5b5debac6b000ce335312c5bec1
JosemVG <josevegra@hotmail.com>
2021-10-22 13:08:35 +0200

    Refactor. Added BLEU, EM Score and ipnyb version

--- test/Practicum/EQAKGMetrics.py
+++ test/Practicum/EQAKGMetrics.py
@@ -4,6 +4,8 @@ import enchant
 import csv
 import re
 import time
+import itertools
+from sacrebleu import sentence_bleu
 
 def jsonToDict(route) -> dict:
     '''
@@ -30,6 +32,18 @@ def queryJSON(queryURL, json):
     #Obtenemos la respuesta como JSonObject y la devolvemos
     return response.json()
 
+def exactMatchScore(string1,string2):
+    '''
+    Funcion auxiliar que incorpora la medida EM (exact match)
+    '''
+    matches = 0
+    total = 0
+    for (x,y) in itertools.zip_longest(string1,string2):
+        if(x == y):
+            matches+=1
+        total+=1
+    return matches/total
+
 def writeResults(csvwriter, question, modelAnswerLong, obtainedAnswer, queryTime, textLen):  
     '''
     Funcion auxiliar que extrae la respuesta que se espera, hace la distancia de levenshtein y escribe en el csv:
@@ -43,7 +57,6 @@ def writeResults(csvwriter, question, modelAnswerLong, obtainedAnswer, queryTime
     #La respuesta esperada se obtiene con una expresion regular (sacar texto entre corchetes)
     modelAnswerLongGroups = re.search(r"\[([^\)]+)\]", modelAnswerLong)
     if(modelAnswerLongGroups is not None):
-
         modelAnswer = modelAnswerLongGroups.group(1)
         isAnswered = "YES"
         if modelAnswer == "answer":
@@ -51,15 +64,18 @@ def writeResults(csvwriter, question, modelAnswerLong, obtainedAnswer, queryTime
         distance = "None"
         if obtainedAnswer is not None:
             distance = enchant.utils.levenshtein(modelAnswer,obtainedAnswer)
-        csvwriter.writerow( [question, modelAnswer, obtainedAnswer, distance , queryTime, textLen, isAnswered] )
+            reference = modelAnswer.split()
+            candidate = obtainedAnswer.split()
 
-def compareAnswersJSON(JSONroute, queryURL, csvRoute):
+        csvwriter.writerow( [question, modelAnswer, obtainedAnswer, distance, sentence_bleu(obtainedAnswer,[modelAnswer]).score, exactMatchScore(reference,candidate), queryTime, textLen, isAnswered] )
+
+def EQAKGMetrics(JSONroute, queryURL, csvRoute):
     '''
     Función que dado un JSON con preguntas y respuestas (asumimos que las preguntas están en la clave 'question' del JSON, y las respuestas en 'verbalized_answers'), 
-    una url a través de la cual realizar consultas y un csv donde guardar los resultados:
+    una url a través de la cual realizar consultas y un csv donde guardar los resultados, hace una serie de metricas:
     - Realiza las preguntas del JSON dado
-    - Lo compara con la respuesta esperada segun la Distancia de Levenshtein
-    - Guarda en el CSV la pregunta, la respuesta esperada, la respuesta obtenida y varias metricas de rendimiento
+    - Lo compara con la respuesta esperada y obtiene varias metricas de rendimiento (Distancia de Levenshtein, BLEU, EM,...)
+    - Guarda en el CSV la pregunta, la respuesta esperada, la respuesta obtenida y estas metricas
     '''
     VQuandaData = jsonToDict(JSONroute)
 
@@ -68,7 +84,8 @@ def compareAnswersJSON(JSONroute, queryURL, csvRoute):
         csvwriter = csv.writer(f,delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
 
         #Escribimos el Header
-        csvwriter.writerow( ["Question", "Answer", "Response", "Levenshtein Distance","Query Time","Text Length","Is Answered"])
+        csvwriter.writerow( ["Question", "Answer", "Response", "Levenshtein Distance","BLEU Score","EM Score","Query Time","Text Length","Is Answered"])
+        counter = 0
 
         for i in VQuandaData:
 
@@ -79,7 +96,12 @@ def compareAnswersJSON(JSONroute, queryURL, csvRoute):
 
             #Pasamos las respuestas a minuscula y llamamos a extractAndCompare.
             writeResults(csvwriter, i['question'], i['verbalized_answer'].lower(),jsonResponse['answer'].lower(),queryTime,jsonResponse['textLen'])
+            counter += 1
+            if(counter % 50 == 0):
+                print("Reached question " + str(counter))
 
         f.close()
-        
-compareAnswersJSON("train.JSON","http://localhost:5000/dbpedia/en","levenshtein.csv")
+        print(counter)
+
+queryUrl = "https://librairy.linkeddata.es/eqakg/dbpedia/en?text=false" 
+EQAKGMetrics("train.JSON",queryUrl,"levenshtein.csv")
