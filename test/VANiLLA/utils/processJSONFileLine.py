e6a095d1ce3eecd10ea70a6b3184c2b748a92f1d
JosemVG <josevegra@hotmail.com>
2021-11-17 11:13:19 +0100

    Upload complete csv. Changes on VANiLLA utils.

--- test/VANiLLA/utils/processJSONFileLine.py
+++ test/VANiLLA/utils/processJSONFileLine.py
@@ -2,25 +2,22 @@ import pandas as pd
 import json
 from pprint import pprint
 
-def processJSON(JSONRoute,JSON):
+def JSONLineToDict(JSONRoute,JSON):
     '''
     df = pd.read_json(JSONRoute, lines=True)
     df.to_json(JSON)
     '''  
 
     with open(JSONRoute) as f:
-        json_list = list(f)
+        jsonList = list(f)
     
-    output_list = json.dumps([json.loads(JSON_STRING) for JSON_STRING in json_list])
-    pprint(output_list)
-    return output_list
+    return json.dumps([json.loads(jsonLine) for jsonLine in jsonList])
 
     '''
     for json_str in json_list:
         result = json.loads(json_str)
         print(f"result: {result}")
     '''
-
-    
-processJSON("Vanilla_Dataset_Test.json","test.json")
+   
+pprint(JSONLineToDict("Vanilla_Dataset_Test.json","test.json"))
 
