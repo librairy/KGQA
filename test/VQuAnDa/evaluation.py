03d8bb3c7d47ed0eeed958cce81de902715d6115
José Manuel Vega Gradit <67061927+Josemvg@users.noreply.github.com>
2021-12-07 14:42:53 +0100

    Fixed evaluation.py for empty strings. Removed mp.

--- test/VQuAnDa/evaluation.py
+++ test/VQuAnDa/evaluation.py
@@ -1,6 +1,5 @@
 import csv
 from sacrebleu import sentence_bleu
-import multiprocessing as mp
 import pandas as pd
 import nltk
 from sklearn.metrics.pairwise import cosine_similarity
@@ -9,7 +8,7 @@ def csvToDict(route) -> dict:
     '''
     Funcion auxiliar que dada la ruta de un csv, lo abre y lo convierte a lista de diccionarios
     '''
-    df = pd.read_csv(route, sep=";")
+    df = pd.read_csv(route, sep=";", na_filter=False)
     return df.to_dict('records')
 
 def exactMatchScore(string1,string2):
@@ -18,38 +17,26 @@ def exactMatchScore(string1,string2):
     Para listas de cadenas, comprueba si ambas contienen los mismos elementos (no importa el orden)
     '''
     if ("," in string1) and ("," in string2):
-        string1 = string1.split(",")
-        string2 = string2.split(",")
+        string1 = string1.split(", ")
+        string2 = string2.split(", ")
         return int((len(string1) == len(string2)) and (set(string1) == set(string2)))
     return int(string1 == string2)
 
-def evaluateQuestion(csvRoute,questionDict,rows,counter):
+def evaluateQuestion(questionDict):
     '''
     Funcion auxiliar que calcula las metricas para una determinada pregunta y las escribe en un CSV en una ruta dada..
     '''
     modelAnswer = questionDict['Answer']
     obtainedAnswer = questionDict['Response']
-    #Tokenizamos la respuesta obtenida y la respuesta modelo
-    reference = modelAnswer.split()
-    candidate = obtainedAnswer.split()
 
     sacreBleuScore = sentence_bleu(obtainedAnswer,[modelAnswer]).score
     bleuScore = nltk.translate.bleu_score.sentence_bleu([modelAnswer], obtainedAnswer)*100
     meteorScore = nltk.translate.meteor_score.meteor_score([[modelAnswer]],[obtainedAnswer])*100
     
-    rows.append( [questionDict['Question'], sacreBleuScore, bleuScore, meteorScore, exactMatchScore(reference,candidate),"placeholder"] )
-    counter.value += 1
-    #print("Contador: ", counter.value)
+    global rows
+    rows.append( [questionDict['Question'], sacreBleuScore, bleuScore, meteorScore, exactMatchScore(modelAnswer,obtainedAnswer),"placeholder"] )
 
-    #Escribimos cuando el valor del contador llegue a 24
-    if(counter.value % 24 == 0):
-        print("Escribiendo. Contador: ", counter.value)
-        with open(csvRoute, 'a', newline='', encoding="utf-8") as f:
-            (pd.DataFrame.from_records(rows, columns=header)).to_csv(f, header=False, index=False, sep=';', quoting=csv.QUOTE_ALL)
-            rows[:] = []
-            f.close()
-
-def evaluator(pool, rows, counter, csvRoute1, csvRoute2, writeHeader = False):
+def evaluator(csvRoute1, csvRoute2, writeHeader = False):
     '''
     Funcion que dado un csv con preguntas, las respuestas obtenidas y las respuestas modelo, hace una serie de metricas (BLEU, EM, METEOR,...)
     '''
@@ -58,34 +45,25 @@ def evaluator(pool, rows, counter, csvRoute1, csvRoute2, writeHeader = False):
     #Escribimos el Header
     if writeHeader:
         with open(csvRoute2,'w', newline='', encoding="utf-8") as f:
-
             csvwriter = csv.writer(f,delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
-            global header
             csvwriter.writerow(header)
             f.close()
         
-    for i in data:
-        #Paraleliza con metodos asincronos
-        pool.apply_async(evaluateQuestion, (csvRoute2,i,rows,counter))
-
-    pool.close()
-    pool.join()
+    for value,i in enumerate(data):
+        evaluateQuestion(i)
+        if value != 0 and value % 24 == 0:
+            with open(csvRoute2, 'a', newline='', encoding="utf-8") as f:
+                global rows
+                (pd.DataFrame.from_records(rows, columns=header)).to_csv(f, header=False, index=False, sep=';', quoting=csv.QUOTE_ALL)
+                rows[:] = []
+                f.close()
 
     #Escribimos lo que quede
     with open(csvRoute2, 'a', newline='', encoding="utf-8") as f:
         (pd.DataFrame.from_records(rows, columns=header)).to_csv(f,header=False, index=False, sep=';', quoting=csv.QUOTE_ALL)
         f.close()
 
+rows = []
 header = ["Question","BLEU","SacreBLEU","Meteor","EM","Cosine Similarity"]
 
-
-if __name__ == '__main__':
-
-    with mp.Manager() as manager:
-
-        rows = manager.list([])
-        counter = manager.Value('i', 0)
-
-        pool = mp.Pool(processes=6, initargs = (counter,rows,))
-
-        evaluator(pool,rows,counter,"results/VQuAnDa.csv","results/VQuAnDa_Evaluation.csv", writeHeader=True)
+evaluator("results/VQuAnDa.csv","results/VQuAnDa_Evaluation.csv", writeHeader=True)
