59dc118fcb736771b5f9478bbac12e1534275936
José Manuel Vega Gradit <67061927+Josemvg@users.noreply.github.com>
2022-01-08 11:10:52 +0100

    Added answer number handling on Interface

--- ui/webapp.py
+++ ui/webapp.py
@@ -3,6 +3,7 @@ from streamlit import cli as stcli
 import requests
 from annotated_text import annotated_text
 import sys
+import operator
 
 def queryJSON(queryURL, question):
     '''
@@ -12,40 +13,47 @@ def queryJSON(queryURL, question):
         'question': (None, question),
     }
     response = requests.get(queryURL, files = files)
-    #pprint(response.json())
-    #Obtenemos la respuesta como JSonObject y la devolvemos
     return response.json()
 
 def main():
 
     st.set_page_config(
-        page_title="MuHeQa",
-        page_icon=":book:",
-        layout="centered",
-        initial_sidebar_state="auto",
+        page_title = "MuHeQa",
+        page_icon = ":book:",
+        layout = "centered",
+        initial_sidebar_state = "auto",
     )
 
     st.title('MuHeQa')
+
     st.subheader('Question Answering over Multiple and Heterogeneous Knowledge Bases')
+    
     st.markdown("""
-    Web service that creates natural language answers from natural language questions using as knowledge base a combination of structured (KG) and unstructured (documents) data.
+    Web Service that creates Natural Language answers from Natural Language questions using as Knowledge base a combination of both structured (KG) and unstructured (documents) data.
     """, unsafe_allow_html=True)
+    
     question = st.text_input('')
-    data = {'question': "",'answerNumber': 5}
-    data['question'] = question
+
+    data = {
+        'question': question,
+        'answerNumber': 10
+    }
 
     st.sidebar.subheader('Options')
-    answerNumber = st.sidebar.slider('How many relevant answers do you want?', min_value=1, max_value=5)
+    answerNumber = st.sidebar.slider('How many relevant answers do you want?', min_value=1, max_value=10)
 
     #Lista de bases de conocimiento sobre las que haremos nuestra consulta
     knowledgeBases = ["dbpedia"]
 
-    @st.cache(show_spinner=False)
+    @st.cache(show_spinner=False, allow_output_mutation=True)
     def getAnswers(data):
         '''
         Funcion auxiliar que obtiene una lista con todas las respuestas sobre las distintas bases de conocimiento
         '''
-        answerList = []
+        answerList = [
+
+        ]
+
         for i in knowledgeBases:
             queryURL = "http://localhost:5000/eqakg/" + i + "/en?text=true"
             answerList.append(queryJSON(queryURL,data["question"]))
@@ -67,20 +75,24 @@ def main():
 
     if question:
         with st.spinner(text=':hourglass: Looking for answers...'):
+            counter = 0
             results = getAnswers(data)
+            results.sort(key = operator.itemgetter('score'), reverse = True)
             for i in results:
+                if counter >= answerNumber:
+                    break
+                counter += 1
                 answer = i['answer']
-                st.write("**Answer: **", answer)
-                #answer_display.subheader(answer)
                 if answer:
-                        context = '...' + i['text'] + '...'
-                        source = "source"
-                        relevance = i['score']
-                        annotateContext(i, answer, context)
-                        '**Relevance:** ', relevance , '**source:** ' , source
+                    st.write("**Answer: **", answer)
+                    context = '...' + i['text'] + '...'
+                    source = "source"
+                    relevance = i['score']
+                    annotateContext(i, answer, context)
+                    '**Relevance:** ', relevance , '**Source:** ' , source
 
     if question and st.sidebar.checkbox('Show debug info'):
-        st.subheader('REST API JSON response')
+        st.subheader('API JSON Response')
         st.write(results)
 
 if __name__ == '__main__':
