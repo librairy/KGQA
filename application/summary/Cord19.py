9378fe1f9587d76a943e8cfda3650ffe914346ac
cbadenes <cbadenes@gmail.com>
2022-02-02 15:39:47 +0100

    handle missing values

--- application/summary/Cord19.py
+++ application/summary/Cord19.py
@@ -10,7 +10,7 @@ class Cord19():
         self.solr = pysolr.Solr(url, search_handler="/select", timeout=5)
         #self.nlp = spacy.blank(lang)
         self.nlp = spacy.load("en_core_web_sm")
-    
+
 
     def get_summary(self,question):
         # extract named entites
@@ -22,35 +22,38 @@ class Cord19():
         chemicals = response['entities']['chemicals']
         diseases = response['entities']['diseases']
         genetics = response['entities']['genetics']
-        
+
         search_filter = []
         for chemical in chemicals:
-            search_filter.append("biobert_chemical_normalized_term:"+chemical['found_term'])
+            if 'found_term' in chemical:
+                search_filter.append("biobert_chemical_normalized_term:"+chemical['found_term'])
         for disease in diseases:
-            search_filter.append("biobert_disease_normalized_term:"+disease['found_term'])
+            if 'found_term' in disease:
+                search_filter.append("biobert_disease_normalized_term:"+disease['found_term'])
         for gene in genetics:
-            search_filter.append("biobert_genetic_normalized_term:"+gene['found_term'])
-        
+            if 'found_term' in gene:
+                search_filter.append("biobert_genetic_normalized_term:"+gene['found_term'])
+
         # extract nouns and verbs
         doc = self.nlp(question)
-        
+
         valid_pos = ["NOUN","VERB"]
         for token in doc:
             if token.is_stop:
                 continue
             if token.pos_ in valid_pos:
-                search_filter.append("text_t:"+token.text)        
-        
+                search_filter.append("text_t:"+token.text)
+
         pprint.pprint(search_filter)
-        
+
         entities = ['']
         text = ""
-        
+
         q = " OR ".join(search_filter)
         fl = "text_t,score"
         fq = ""
         rows = 5
-        
+
         results = self.solr.search(q, **{
             'fl': fl,
             'fq': fq,
@@ -59,13 +62,11 @@ class Cord19():
         #print("Number of hits: {0}".format(len(results)))
         for result in results:
           text += result['text_t'] + " "
-        
+
         return text
-    
+
 
 if __name__ == "__main__" :
-    
+
     cord19 = Cord19("en")
     cord19.get_summary("what is the effect of chloroquine on thrombosis?")
-    
-
