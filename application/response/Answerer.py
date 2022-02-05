557de67c8a97bf5d9354165237fde5d7956a5cf2
cbadenes <cbadenes@gmail.com>
2022-02-05 02:23:14 +0100

    return only reponse

---
+++ application/response/Answerer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 12:05:15 2021
+
+@author: cbadenes
+"""
+
+class Answerer:
+
+    def __init__(self):
+        pass
+
+
+    def get_response(self, question, answer):
+        return answer
