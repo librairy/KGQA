0f566d22750884f71bf3395d953475e7bc830476
cbadenes <cbadenes@gmail.com>
2022-02-05 02:35:41 +0100

    training data to verbalize kgs

---
+++ application/summary/kg/nlg/webnlg2csv.py
@@ -0,0 +1,35 @@
+# https://towardsdatascience.com/data-to-text-generation-with-t5-building-a-simple-yet-advanced-nlg-model-b5cce5a6df45
+import glob
+import os
+import re
+import xml.etree.ElementTree as ET
+import pandas as pd
+files = glob.glob(os.getcwd() + "/webnlg/release_v3.0/en/train/**/*.xml", recursive=True)
+triple_re=re.compile('(\d)triples')
+data_dct={}
+for file in files:
+    tree = ET.parse(file)
+    root = tree.getroot()
+    triples_num=int(triple_re.findall(file)[0])
+    for sub_root in root:
+        for ss_root in sub_root:
+            strutured_master=[]
+            unstructured=[]
+            for entry in ss_root:
+                unstructured.append(entry.text)
+                strutured=[triple.text for triple in entry]
+                strutured_master.extend(strutured)
+            unstructured=[i for i in unstructured if i.replace('\n','').strip()!='' ]
+            strutured_master=strutured_master[-triples_num:]
+            strutured_master_str=(' && ').join(strutured_master)
+            data_dct[strutured_master_str]=unstructured
+mdata_dct={"prefix":[], "input_text":[], "target_text":[]}
+for st,unst in data_dct.items():
+    for i in unst:
+        mdata_dct['prefix'].append('webNLG')
+        mdata_dct['input_text'].append(st)
+        mdata_dct['target_text'].append(i)
+
+
+df=pd.DataFrame(mdata_dct)
+df.to_csv('webNLG2020_train.csv')
