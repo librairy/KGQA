d186c731d63880fc0b721f01954170b24a826ab0
cbadenes <cbadenes@gmail.com>
2022-02-05 02:23:51 +0100

    handle multiple steps configuration

--- application/workflow.py
+++ application/workflow.py
@@ -1,32 +1,19 @@
-from flask import Flask, request, abort, jsonify, send_from_directory#, session,
-from logging.handlers import RotatingFileHandler
-from application import config
-from flask_cors import CORS
-
-import application.summary.DBpediaEN as dbpedia_en
-import application.summary.DBpediaES as dbpedia_es
-import application.summary.WikidataEN as wikidata_en
-import application.summary.WikidataES as wikidata_es
-import application.summary.Cord19EN as cord19_en
-import application.extraction.BertEN as bert_en
-import application.extraction.RobertaCovidEN as roberta_covid_en
-import application.extraction.RobertaEN as roberta_en
-import application.response.AnswererEN as answerer_en
-import json
 
 class Workflow:
 
-    def __init__(self):
-        print("MuHeQA workflow ready")
+    def __init__(self, summary_components, evidence_component, answer_component):
+        self.summary_components = summary_components
+        self.evidence_component = evidence_component
+        self.answer_component = answer_component
+        print("MuHeQA workflow",self.summary_components,self.evidence_component,self.answer_component,"ready")
 
     def decapitalize(self,str):
         return str[:1].lower() + str[1:]
 
 
-    def process(self,request,summarizer_list,extractive_qa,response_builder):
+    def process(self,request):
         question = request['question']
         print("Making question:",question,"..")
-
 
         entity_list = []
         if 'entities' in request:
@@ -35,7 +22,6 @@ class Workflow:
                 values = e.split(";")
                 entity_list.append({ 'id': values[0], 'name': values[1]})
 
-
         req_evidence = False
         if ('evidence' in request):
             req_evidence = request['evidence']
@@ -43,29 +29,24 @@ class Workflow:
         # Compose Summary
         question = self.decapitalize(question)
         summary = ""
-        for summarizer in summarizer_list:
-            partial_summary = ""
-            if (len(entity_list)==0):
-                partial_summary = summarizer.get_summary(question)
-            else:
-                partial_summary = summarizer.get_summary_from_entities(question,entity_list)
-            summary += partial_summary + " "
+        for summarizer in self.summary_components:
+            partial_summary = summarizer.get_summary(question, entity_list)
+            summary += partial_summary + ". "
 
         # Extract Answer
-        answer = extractive_qa.get_answer(question,summary)
+        answer = self.evidence_component.get_answer(question,summary)
 
         # Create Reponse
-        value = response_builder.get_response(question, answer['value'])
+        response = self.answer_component.get_response(question, answer['value'])
 
         # Return value
-        response = {}
-        response['question'] = question
-        response['answer'] = value[0]
-        response['confidence'] = answer['score']
-        response['result'] = value[1]
-        if req_evidence.lower() == 'true':
-            response['evidence'] = answer['summary']
+        result = {}
+        result['question'] = question
+        result['answer'] = response
+        result['confidence'] = answer['score']
+        if (str(req_evidence).lower() == 'true'):
+            result['evidence'] = answer['summary']
 
-        print("Response: ", response['answer'])
+        print("Response: ", result['answer'])
 
-        return response
+        return result
